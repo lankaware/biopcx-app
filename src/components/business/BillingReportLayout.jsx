8b83c6c1fe395526998ad65e228f74e08b61a450
Celso Vanderlei <celso.vanderlei@promptinf.com.br>
2022-07-05 17:52:52 -0300

    Ajustes faturamento

--- src/components/business/BillingReportLayout.jsx
+++ src/components/business/BillingReportLayout.jsx
@@ -2,102 +2,25 @@ import React from "react";
 import { prettyDate } from '../../services/dateutils'
 
 const RepSalespersonLayout = React.forwardRef((props, refs) => {
-  let currentMonth = ''
   let totalValue = 0
-  let printValue = 0
 
+  let currentMonth = ''
+  let printValue = 0
   let currentSalesperson = ''
   let personValue = 0
   let printPersonValue = 0
   let printSalesperson = ''
 
-  const totalMonth = (month, person, valor) => {
-    if (currentMonth === '') {
-      currentMonth = month
-    }
-    if (currentMonth !== month || currentSalesperson !== person) {
-      printValue = totalValue
-
-      personValue = personValue + totalValue
-
-      totalValue = valor || 0
-      currentMonth = month
-
-      if (printValue !== 0) {
-        return (
-          <>
-            <tr key={10001}>
-              <td style={{ 'width': '30px' }}></td>
-              <td style={{ 'width': '30px' }}></td>
-              <td style={{ 'width': '30px' }}></td>
-              <td style={{ 'textAlign': 'left', 'width': '120px', 'fontWeight': 'bold' }}>Total no Mês</td>
-              {/* <td style={{ 'width': '30px' }}></td> */}
-              <td style={{ 'textAlign': 'right', 'fontWeight': 'bold' }}>{printValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-            </tr>
-            <tr key={10002}><td>&nbsp;</td></tr>
-          </>
-        )
-      }
-    } else {
-      totalValue = totalValue + valor || 0
-    }
-  }
-
-  const totalPerson = (person, valor) => {
-    if (currentSalesperson === '') {
-      currentSalesperson = person
-      return (
-        <>
-          <tr key={10003}>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'textAlign': 'left', 'width': '120px', 'fontWeight': 'bold' }}>Vendedor: {person}</td>
-            {/* <td style={{ 'width': '120px' }}>{person}</td> */}
-          </tr>
-        </>
-      )
-    }
-    if (currentSalesperson !== person) {
-      printPersonValue = personValue
-      printSalesperson = currentSalesperson
-
-      personValue = 0
-      currentSalesperson = person
-
-      const nextPerson = (person !== '' ? 'Vendedor: ' + person : '')
-      return (
-        <>
-          <tr key={10004}>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'textAlign': 'left', 'width': '280px', 'fontWeight': 'bold' }}>Total do Vendedor {printSalesperson}</td>
-            {/* <td style={{ 'width': '30px' }}></td> */}
-            <td style={{ 'textAlign': 'right', 'fontWeight': 'bold' }}>{printPersonValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-          </tr>
-          <tr key={10005}><td>&nbsp;</td></tr>
-          <tr key={10006}>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'width': '30px' }}></td>
-            <td style={{ 'textAlign': 'left', 'width': '120px', 'fontWeight': 'bold' }}>{nextPerson}</td>
-          </tr>
-
-        </>
-      )
-    }
-  }
-
   const personTitle = () => {
     return (props.billcovenant === '*' ? '(todos)' : props.billcovenant)
   }
-  
+
   return (
     <div ref={refs} style={{ 'marginLeft': '20px' }}>
+      <h1>&nbsp;</h1>
       <h4>Biopace</h4>
-      <h4>Convênio: {personTitle()} </h4>
-      <h4>Período: {prettyDate(props.dateInit)} a {prettyDate(props.dateEnd)}</h4>
+      <h5>Convênio: {personTitle()} </h5>
+      <h5>Período: {prettyDate(props.dateInit)} a {prettyDate(props.dateEnd)}</h5>
       {/* <h3>&nbsp;</h3> */}
       <table>
         <thead>
@@ -115,10 +38,9 @@ const RepSalespersonLayout = React.forwardRef((props, refs) => {
         <tbody>
           <tr key={10008}><td>&nbsp;</td></tr>
           {props.list.map((item) => {
+            totalValue += item.amount
             return (
               <>
-                {/* {totalMonth(item.DATA.substr(5, 2), item.VENDEDOR, item.TOTAL)}
-                {totalPerson(item.VENDEDOR, item.TOTAL)} */}
                 <tr key={item._id}>
                   <td style={{ 'textAlign': 'rileft' }}>{prettyDate(item.attendanceDate)}</td>
                   <td style={{ 'textAlign': 'left' }}>{item.patient_name}</td>
@@ -130,10 +52,14 @@ const RepSalespersonLayout = React.forwardRef((props, refs) => {
               </>
             )
           })}
-          {totalMonth('', 0)}
-          {totalPerson('', 0)}
+          <tr key={10009}><td>&nbsp;</td></tr>
+          <tr key={10010}><td>&nbsp;</td></tr>
+          {/* <tr key={10009}>
+            <td style={{ 'textAlign': 'left', 'fontWeight': 'bold' }}>Valor Total: {totalValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+          </tr> */}
         </tbody>
       </table>
+      <h5>Valor Total: {totalValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h5>
     </div>
   )
 })
